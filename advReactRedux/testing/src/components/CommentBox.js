8803b43fe321369b34af94a29677bd507f16ac0f
hublaw <ldoireann@gmail.com>
2018-09-28 22:45:16 -0400

    [advReactRed] First commit

---
+++ advReactRedux/testing/src/components/CommentBox.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+export default class CommentBox extends Component {
+  state = { comment: ''};
+
+  handleChange = (event) => {
+    this.setState({comment: event.target.value})
+  }
+
+  handleSubmit = (event) => {
+    // prevents page reloading when submitted
+    event.preventDefault();
+
+    // TODO: Call an action creator and save the comment
+    this.setState({comment: ''});
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add a comment</h4>
+        <textarea value={this.state.comment} onChange={this.handleChange} />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+    );
+  }
+}
---
+++ advReactRedux/testing/src/components/CommentList.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class CommentList extends Component {
+  render() {
+    return (
+      <div>I am the CommentList</div>
+    );
+  }
+}
